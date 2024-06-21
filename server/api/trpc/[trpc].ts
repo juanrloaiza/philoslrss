aa8fc6d6e22b016f3b1ecdd45cf000d5820ec1d9
Juan R. Loaiza <juanrloaiza@gmail.com>
2024-06-21 16:41:10 -0400

    Implements take 10 on API

--- server/api/trpc/[trpc].ts
+++ server/api/trpc/[trpc].ts
@@ -1,7 +1,3 @@
-/**
- * This is the API-handler of your app that contains all your API routes.
- * On a bigger app, you will probably want to split this file up into multiple files.
- */
 import { createNuxtApiHandler } from 'trpc-nuxt'
 import { publicProcedure, router } from '~/server/trpc/trpc'
 import { PrismaClient } from '@prisma/client'
@@ -10,10 +6,12 @@ const prisma = new PrismaClient()
 
 export const appRouter = router({
     events: publicProcedure
-        .query(() => {
-            // This is what you're returning to your client
-            return prisma.event.findMany();
-        })
+        .query(() => prisma.event.findMany(
+            {
+                take: 10,
+                orderBy: { pubDate: 'desc' }
+            }
+        ))
 })
 
 export type AppRouter = typeof appRouter;
