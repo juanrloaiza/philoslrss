f80bc2e888a2335e9179e21a94e6186b4bf5da4a
Juan R. Loaiza <juanrloaiza@gmail.com>
2024-06-21 22:48:28 -0400

    Implements a rough loading button

--- app.vue
+++ app.vue
@@ -2,24 +2,38 @@
 import { ref } from "vue";
 
 const { $client } = useNuxtApp();
-const { status, data: events } = await useLazyAsyncData(() => $client.events.query({skip:0}))
+const { status, data: events } = await useLazyAsyncData(
+  () => $client.events.query({skip:0}))
 
 let offset = 0
 const batchSize = 3
 const canGetMore = ref(true)
-let newStatus = ""
+
+const loadingButtonData = {
+  text: ref('Load more'),
+  isLoading: ref(false)
+}
+
 const getMoreEvents = async() => {
   offset = offset + batchSize;
-  const {status: newStatus, data: newEvents} = await useLazyAsyncData(
+
+  loadingButtonData.text.value = "Loading..."
+  loadingButtonData.isLoading.value = true
+
+  const {data: newEvents} = await useLazyAsyncData(
     () => $client.events.query({skip: offset})
   )
+  
   if (!newEvents.value) {
     return
   }
+
   events.value?.push(...newEvents.value)
   canGetMore.value = (newEvents.value.length == batchSize)
+
+  loadingButtonData.text.value = "Load more"
+  loadingButtonData.isLoading.value = false
 }
-const a = ref(newStatus)
 
 </script>
 
@@ -27,22 +41,32 @@ const a = ref(newStatus)
   <div class="bg-slate-100 flex flex-col items-center py-8">
     <div class="container">
       <div class="text-center mb-8">
-    <h1 class="drop-shadow-2xl text-4xl font-bold mb-4">Philos-L List Archive (RSS)</h1>
-    <p class="text-xl">A visualization of the latest posts in the Philos-L mailing list.</p>
+    <h1 class="drop-shadow-2xl text-4xl font-bold mb-4">
+      Philos-L List Archive (RSS)
+    </h1>
+    <p class="text-xl">
+      A visualization of the latest posts in the Philos-L mailing list.
+    </p>
   </div>
     <div v-if="status == 'pending'" class="flex justify-center">
       <LoadingSpinner />
     </div>
-    <div v-auto-animate  class='flex flex-col items-center'  v-else>
+    <div v-auto-animate class='flex flex-col items-center' v-else>
       <div v-auto-animate class="flex flex-wrap gap-5 justify-center">
-      <EventsCard v-for="event in events" :key="event.id" 
-      v-bind="event" />
+      <EventsCard 
+        v-for="event in events" 
+        :key="event.id" 
+        v-bind="event" 
+      />
       </div>
-      <button
-    class="m-6 p-3 border-2 rounded border-slate-300 bg-white hover:bg-slate-100"
-    v-if="canGetMore" @click="getMoreEvents()">
-     Load more {{ a }}
-  </button>
+    <button 
+      v-if="canGetMore" 
+      :class="{'hover:opacity-50 opacity-50': loadingButtonData.isLoading.value == true }"
+      class="m-6 p-3 border-2 rounded border-slate-300 bg-white hover:bg-slate-100"
+      @click="getMoreEvents()"
+      >
+      {{ loadingButtonData.text.value }}
+    </button>
     </div>
   </div></div>
 </template>
