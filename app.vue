4582194e025d6a21580a1ecf158ffaff9ecf8ac3
Juan R. Loaiza <juanrloaiza@gmail.com>
2024-06-21 22:10:39 -0400

    Creates Event Card and animations

--- app.vue
+++ app.vue
@@ -1,47 +1,48 @@
 <script setup lang="ts">
-import moment from "moment";
+import { ref } from "vue";
 
 const { $client } = useNuxtApp();
-const { data } = await $client.events.useLazyQuery()
+const { status, data: events } = await useLazyAsyncData(() => $client.events.query({skip:0}))
 
-const formatDate = (date: string) => {
-  return moment(date).format('DD.MM.YY')
+let offset = 0
+const batchSize = 3
+const canGetMore = ref(true)
+let newStatus = ""
+const getMoreEvents = async() => {
+  offset = offset + batchSize;
+  const {status: newStatus, data: newEvents} = await useLazyAsyncData(
+    () => $client.events.query({skip: offset})
+  )
+  if (!newEvents.value) {
+    return
+  }
+  events.value?.push(...newEvents.value)
+  canGetMore.value = (newEvents.value.length == batchSize)
 }
+const a = ref(newStatus)
 
 </script>
 
 <template>
-  <div class="bg-gradient-to-b from-white to-blue-200 bg-fixed flex flex-col items-center py-8">
+  <div class="bg-slate-100 flex flex-col items-center py-8">
     <div class="container">
       <div class="text-center mb-8">
     <h1 class="drop-shadow-2xl text-4xl font-bold mb-4">Philos-L List Archive (RSS)</h1>
     <p class="text-xl">A visualization of the latest posts in the Philos-L mailing list.</p>
   </div>
-    <div v-if="!data" class="flex justify-center">
+    <div v-if="status == 'pending'" class="flex justify-center">
       <LoadingSpinner />
     </div>
-    <div class=" " v-else>
-      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 justify-center">
-      <div v-for="event in data" :key="event.id" class="bg-white p-6 border rounded text-ellipsis overflow-hidden shadow-lg">
-        <h2 class="text-xl font-bold hover:text-slate-600 mb-2">
-          <NuxtLink :href="event.link">{{ event.title }}</NuxtLink>
-        </h2>
-        <div class="font-semibold">
-          {{ event.creator }}
-        </div>
-        <div class="is-size-7"> {{ formatDate(event.pubDate) }}</div>
-        <div class="my-3"><span v-html="event.content"></span>
-        </div>
+    <div v-auto-animate  class='flex flex-col items-center'  v-else>
+      <div v-auto-animate class="flex flex-wrap gap-5 justify-center">
+      <EventsCard v-for="event in events" :key="event.id" 
+      v-bind="event" />
       </div>
+      <button
+    class="m-6 p-3 border-2 rounded border-slate-300 bg-white hover:bg-slate-100"
+    v-if="canGetMore" @click="getMoreEvents()">
+     Load more {{ a }}
+  </button>
     </div>
-    </div>
-  </div>
-  </div>
+  </div></div>
 </template>
-
-<style scoped>
-.event{
-  border: 1px solid #ccc;
-  border-radius: 10px;
-}
-</style>
---
+++ components/EventsCard.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import moment from "moment";
+
+const event = defineProps({
+    title: String,
+    creator: String,
+    content: String,
+    pubDate: String,
+    link: String
+})
+
+const formatDate = (date: string) => {
+  return moment(date).format('DD.MM.YY')
+}
+</script>
+
+<template>
+
+<div class="flex-none w-1/4 bg-white p-6 border rounded text-ellipsis overflow-hidden shadow-lg">
+        <h2 class="text-xl font-bold hover:text-slate-600 mb-2">
+          <NuxtLink :href="event.link">{{ event.title }}</NuxtLink>
+        </h2>
+        <div class="font-semibold">
+          {{ event.creator }}
+        </div>
+        <div class="is-size-7"> {{ formatDate(event.pubDate!) }}</div>
+        <div class="my-3"><span v-html="event.content"></span>
+        </div>
+      </div>
+</template>
