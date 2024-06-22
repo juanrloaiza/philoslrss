da91b15bc9c1f4284b57b04a21dd8ef194463ed5
Juan R. Loaiza <juanrloaiza@gmail.com>
2024-06-21 22:58:22 -0400

    Loading button component

---
+++ components/LoadingButton.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const props = defineProps({
+    isLoading: Boolean,
+})
+</script>
+
+<template>
+    <button 
+      :class="{'hover:opacity-50 opacity-50': isLoading }"
+      class="m-6 p-3 border-2 rounded border-slate-300 bg-white hover:bg-slate-100">
+      {{ isLoading ? "Loading..." : "Load more"}}
+    </button>
+
+</template>
